58fe3ced9ca1275eb483f2aeb9237884895c7cbc
Juan Pablo Enrici <jpenrici@gmail.com>
2021-11-09 20:27:10 -0300

    Update.

--- src/create_svg_pixelart.py
+++ src/create_svg_pixelart.py
@@ -6,8 +6,10 @@
 
 import os
 import sys
+import time
 import numpy as np
 from PIL import Image
+
 
 PX = 25.4 / 96
 TARGETS = {
@@ -30,6 +32,17 @@ RECTANGLE = """    <rect
        x="#X0#"
        y="#Y0#" />
 """
+GROUP="""   <g
+     id="g_#ID#" >
+#SHAPE#
+   </g>
+"""
+
+LIMIT = 500      # recursion
+
+nparray = []     # image
+visited = []     # pixels
+connected = []   # region
 
 
 def load(path):
@@ -70,9 +83,55 @@ def dec2hex(dec):
     return d[r] + d[n]
 
 
+def conect(x, y, fill, pixel_by_group):
+    global nparray, visited, connected
+    height, width, channels = nparray.shape
+
+    if (pixel_by_group > LIMIT):
+        return
+
+    if x < 0 or x >= (width - 1) or y < 0 or y >= (height - 1):
+        return
+
+    if not visited[y, x]:
+        # Color Models
+        if channels == 4:
+            r, g, b, a = nparray[y, x]
+            if a != 255:
+                visited[y, x] = True
+                return
+        if channels == 3:
+            r, g, b = nparray[y, x]
+        # Hexadecimal color
+        if fill != "#" + dec2hex(r) + dec2hex(g) + dec2hex(b) :
+            return
+        # Store
+        connected += [{'x': x, 'y': y, 'fill': fill}]
+        # Update
+        visited[y,x] = True
+        pixel_by_group += 1
+
+        # Recursion
+        conect(x + 1, y, fill, pixel_by_group)      # right
+        conect(x, y + 1, fill, pixel_by_group)      # down
+        conect(x - 1, y, fill, pixel_by_group)      # left
+        conect(x, y - 1, fill, pixel_by_group)      # up
+        conect(x + 1, y + 1, fill, pixel_by_group)  # right, down
+        conect(x - 1, y + 1, fill, pixel_by_group)  # left, down
+        conect(x + 1, y - 1, fill, pixel_by_group)  # right, up
+        conect(x - 1, y - 1, fill, pixel_by_group)  # left, up
+
+
 def create(image_path):
+    global nparray, visited, connected
+
+    # Inform
+    start = time.time()
+    print("Attention: Slow algorithm for large images!")
+
     # Load image
     try:
+        print("Loaded: {}".format(image_path))
         img = Image.open(image_path)
         nparray = np.array(img)
         height, width, channels = nparray.shape
@@ -102,26 +161,48 @@ def create(image_path):
     base = base.replace("#PX#", str(TARGETS["#PX#"] * PX))
     base = base.replace("#CS#", TARGETS["#CS#"]) 
 
+    print("Start processing ...")
+
     # Create pixels
+    counter = 0
     rectangles = ""
+    visited = np.zeros((height, width))
     for x in range(0, width):
         for y in range(0, height):
+            if visited[y, x]:
+                continue
             # Color Models
             if channels == 4:
                 r, g, b, a = nparray[y, x]
                 if a != 255:
+                    visited[y, x] = True
                     continue
             if channels == 3:
                 r, g, b = nparray[y, x]
             # Hexadecimal color
             fill = "#" + dec2hex(r) + dec2hex(g) + dec2hex(b)
             # SVG
-            s = base
-            s = s.replace("#CF#", fill)
-            s = s.replace("#ID#", TARGETS["#ID#"] + str(y * width + x))
-            s = s.replace("#X0#", str(x * TARGETS["#PX#"] * PX))
-            s = s.replace("#Y0#", str(y * TARGETS["#PX#"] * PX))
-            rectangles += s
+            connected = []
+            conect(x, y, fill, 0)
+            if len(connected) == 0:
+                continue
+            temp = ""
+            for pixel in connected:
+                s = base
+                s = s.replace("#CF#", pixel['fill'])
+                s = s.replace("#ID#", TARGETS["#ID#"] + str(pixel['y'] * width + pixel['x']))
+                s = s.replace("#X0#", str(pixel['x'] * TARGETS["#PX#"] * PX))
+                s = s.replace("#Y0#", str(pixel['y'] * TARGETS["#PX#"] * PX))
+                temp += s
+            if len(connected) > 1:
+                g = GROUP.replace("#ID#", str(counter))
+                temp = g.replace("#SHAPE#", temp)
+                counter += 1
+            rectangles += temp
+
+    # Time
+    end = time.time()
+    print("Finished! Time: {0:.2f} seconds".format(end - start))
 
     # Save
     svg = svg.replace(MARKCODE, rectangles)
@@ -143,7 +224,7 @@ if __name__ == '__main__':
     # Test
     # create("../images/image_test_1.png")
     # create("../images/image_test_2.jpg")
-    # create("../images/bonsai.png")    
+    # create("../images/bonsai.png")
 
     # Run 
     main(sys.argv)
